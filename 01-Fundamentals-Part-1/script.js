3abb692c31a02cf73bb7c945890db26c83565796
himalraj07 <himalrajbhusal@gmail.com>
2024-01-14 20:35:24 +0545

    Values and Variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,5 +1,25 @@
 let js = "amazing";
-if (js == "amazing") alert("JavaScript is FUN!");
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Himal");
+console.log("23");
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let himal_nabin = "JM";
+let $function = 27;
+
+let person = "Himal";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+console.log(myFirstJob);
