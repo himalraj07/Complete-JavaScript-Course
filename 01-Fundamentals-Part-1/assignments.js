c765cce4d87a27e16e5fca7ac393196b00504e67
himalraj07 <himalrajbhusal@gmail.com>
2024-01-14 22:36:48 +0545

    Data Types

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,29 @@
+/*
+// LECTURE: VALUES AND VARIABLES  
+// 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions)
+// 2. Log their values to the console
+
+let country = "Nepal";
+let continent = "Asia";
+let population = 31081086;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+/*
+// LECTURE: DATA TYPES
+// 1. Declare a variable called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable'language', but don't assign it any value yet
+// 2. Log the types of 'isIsland', 'population', 'country' and 'language' to the console
+
+let country = "Nepal";
+let population = 31081086;
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+*/
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -10,6 +11,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Variable name conventions
 let himal_nabin = "JM";
 let $function = 27;
 
@@ -23,3 +25,27 @@ let job1 = "Programmer";
 let job2 = "Teacher";
 
 console.log(myFirstJob);
+*/
+
+
+// DATA TYPES
+/*
+let javaScritpIsFun = true;
+console.log(javaScritpIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScritpIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Himal");
+
+javaScritpIsFun = "YES!";
+console.log(typeof javaScritpIsFun);
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+*/
