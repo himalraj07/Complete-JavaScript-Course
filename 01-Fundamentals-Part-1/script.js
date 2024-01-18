86d5d83a7055dbe829756f4f0cdb2d02053857bd
himalraj07 <himalrajbhusal@gmail.com>
2024-01-18 17:32:20 +0545

    Logical Operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -295,3 +295,25 @@ if (favourite === 23) {
 
 if(favourite !== 23) console.log("Why not 23?");
 */
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // A
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive...");
+// }
+
+const isTired = true; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Sarah is able to drive!");
+} else {
+  console.log("Someone else should drive...");
+}
