f3860379e81c8bea59efbc5033097a1ef2df7d8a
himalraj07 <himalrajbhusal@gmail.com>
2024-01-19 15:50:53 +0545

    Reviewing Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -68,7 +68,7 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 
 console.log(yearsUntilRetirement(1991, "Himal"));
 console.log(yearsUntilRetirement(1980, "Bob"));
-*/
+
 
 // FUNCTIONS CALLING OTHER FUNCTIONS
 
@@ -84,3 +84,25 @@ function fruitProcessor(apples, oranges) {
   return juice;
 }
 console.log(fruitProcessor(2, 3));
+*/
+
+// REVIEWING FUNCTIONS
+
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+};
+
+console.log(yearsUntilRetirement(1991, "Himal"));
+console.log(yearsUntilRetirement(1950, "Mike"));
