a8cd652a702c646ed72b17a0844008f5075d13b1
himalraj07 <himalrajbhusal@gmail.com>
2024-01-19 09:54:03 +0545

    functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,3 +1,5 @@
+/*
+// ACTIVATING STRICT MODE
 "use strict";
 
 let hasDriversLicense = false;
@@ -8,3 +10,28 @@ if (hasDriversLicense) console.log("I can drive :D");
 
 // const interface = "Audio";
 // const private = 534;
+
+// FUNCTIONS
+function logger() {
+  console.log("My name is Himal");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+*/
+// FUNCTION DECLARATIONS VS. EXPRESSIONS
