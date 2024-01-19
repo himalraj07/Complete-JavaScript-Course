d7d6f2fa64a10b793eeb23733db0e0701f01c341
himalraj07 <himalrajbhusal@gmail.com>
2024-01-19 10:33:32 +0545

    Arrow Functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -33,7 +33,7 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number("23");
-*/
+
 
 // FUNCTION DECLARATIONS VS. EXPRESSIONS
 
@@ -50,3 +50,21 @@ const calcAge2 = function (birthYear) {
 const age2 = calcAge2(1991);
 
 console.log(age1, age2);
+*/
+
+// ARROW FUNCTIONS
+
+// Arrow function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Himal"));
+console.log(yearsUntilRetirement(1980, "Bob"));
