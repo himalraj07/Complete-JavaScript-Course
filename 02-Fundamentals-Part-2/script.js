0915ef0ef7057aa4ec1f3c524a197a7442a2ae1d
himalraj07 <himalrajbhusal@gmail.com>
2024-01-19 18:32:27 +0545

    Introduction to Objects

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -249,7 +249,7 @@ TEST DATA: 125, 555 and 44
 HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
 
 GOOD LUCK 😀
-*/
+
 
 const calcTip = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
@@ -263,3 +263,22 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 console.log(bills, tips, totals);
+*/
+
+// INTRODUCTION TO OBJECTS
+
+const himalArray = [
+  "Himal",
+  "Bhusal",
+  2037 - 1991,
+  "student",
+  ["Micheal", "Peter", "Steven"],
+];
+
+const himal = {
+  firstName: "Himal",
+  lastName: "Bhusal",
+  age: 2037 - 1991,
+  job: "student",
+  friends: ["Micheal", "Peter", "Steven"],
+};
