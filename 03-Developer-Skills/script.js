c5c052e30fdcc114e087569dd57185d7bad47c43
himalraj07 <himalrajbhusal@gmail.com>
2024-01-21 21:05:57 +0545

    DEBUGGING WITH THE CONSOLE AND BREAKPOINTS

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,6 +1,7 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/*
 const x = '23';
 if (x === 23) console.log(23);
 
@@ -73,3 +74,52 @@ const calcTempAmplitudeNew = function (t1, t2) {
 
 const aplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
 console.log(aplitudeNew);
+*/
+
+// DEBUGGING WITH THE CONSOLE AND BREAKPOINTS
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'cels',
+
+    // C) FIX
+    // value: Number(prompt('Degrees celsius: ')),
+    value: 10,
+  };
+
+  // B) FIND
+  console.table(measurement);
+
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) IDENTIFY
+console.log(measureKelvin());
+
+// Using a dubugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const aplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTITY
+console.log(aplitudeBug);
