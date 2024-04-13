06865968ad553fc77f587f62caf7bf06cca03bfa
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2024-04-13 11:45:26 +0545

    Short Circuiting (&& and ||)

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -54,6 +54,38 @@ const restaurant = {
   },
 };
 
+console.log('--- OR ---');
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Himal');
+console.log('' || 'Himal');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('--- AND ---');
+console.log(0 && 'Himal');
+console.log(7 && 'Himal');
+
+console.log('Hello' && 23 && null && 'Himal');
+
+// Practical example
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+
+/*
+////////////////////////////////////////////
+// Rest Pattern and Parameters
 // 1) Destructuring
 
 // SPREAD, because on RIGHT side of =
