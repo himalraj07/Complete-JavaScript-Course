6683bff6fdf9f86616b627161d8552dccdd2f6fb
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2024-05-11 10:34:07 +0545

    Working with Strings - Part 1

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -54,6 +54,45 @@ const restaurant = {
   },
 };
 
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') console.log('You got the middle seat 😁');
+  else console.log('You got lucky 😎');
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
+console.log(new String('Himal'));
+console.log(typeof new String('Himal'));
+
+console.log(typeof new String('Himal').slice(1));
 ///////////////////////////////////////
 // Coding Challenge #3
 
@@ -83,6 +122,7 @@ const gameEvents = new Map([
   [92, '🔶 Yellow card'],
 ]);
 
+/*
 // 1.
 const events = [...new Set(gameEvents.values())];
 console.log(events);
