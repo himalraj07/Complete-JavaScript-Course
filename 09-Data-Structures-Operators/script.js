fda22336fcb56a4b36b80fdcdfef14cc46b429e7
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2024-05-12 17:52:19 +0545

    Working With Strings - Part 3

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -54,6 +54,59 @@ const restaurant = {
   },
 };
 
+// Split and joinn
+console.log('a+very+nice+string'.split('+'));
+console.log('Himal Raj Bhusal'.split(' '));
+
+const [firstName, lastName] = 'Himal Raj Bhusal'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('himal raj bhusal');
+
+// Padding
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('Himal'.padStart(20, '+').padEnd(30, '+'));
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(64637836));
+console.log(maskCreditCard(73378463864647384));
+console.log(maskCreditCard('334859493847755774747'));
+
+// Repeat
+const message2 = 'Bad weather... All Departues Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'✈️'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+/*
+///////////////////////////////////////////////
+// Working With Strings - Part 2
+
 const airline = 'TAP Air Portugal';
 
 console.log(airline.toLowerCase());
