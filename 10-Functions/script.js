cc0f2700bcf5882f8e30d0d8b23a1992f491fd1c
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2024-06-17 17:28:12 +0545

    How Passing Arguments Works: Value vs. Reference

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const bookings = [];
 
 const createBooking = function (
@@ -26,3 +27,36 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+*/
+
+const flight = 'LH234';
+const himal = {
+  name: 'Himal Raj Bhusal',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, himal);
+// console.log(flight);
+// console.log(himal);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = himal;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(himal);
+checkIn(flight, himal);
