6726c59b5e3d1efb4c6fbe8dfc7d30d9d7b2a20a
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2024-06-18 09:18:06 +0545

    functions accepting callback functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -64,3 +64,31 @@ checkIn(flight, himal);
 
 // FIRST -CLASS AND HIGHER-ORDER FUNCTIONS
 
+// FUNCTIONS ACCEPTING CALLBACK FUNCTIONS
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+
+['Himal', 'Bikash', 'Sagar'].forEach(high5);
