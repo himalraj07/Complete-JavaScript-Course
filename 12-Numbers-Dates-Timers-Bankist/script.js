eda2f66d9e42f788a07451df5882dab6a9d84521
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-02-19 13:32:29 +0545

    working with bigint

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -361,7 +361,7 @@ labelBalance /
     })
   })
 
-*/
+
 
 // 287,460,000,000
 const diameter = 287_460_000_000
@@ -378,3 +378,36 @@ console.log(PI)
 
 console.log(Number('230_000'))
 console.log(parseInt('230_000'))
+
+*/
+
+console.log(2 ** 53 - 1)
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(2 ** 53 + 1)
+console.log(2 ** 53 + 2)
+console.log(2 ** 53 + 3)
+console.log(2 ** 53 + 4)
+
+console.log(123532614256253152684521625421365875125n)
+console.log(BigInt(123532614))
+
+// Operations
+console.log(10000n + 10000n)
+console.log(6465446464544484464541542215456651n * 100000000000n)
+// console.log(Math.sqrt(16n))
+
+const huge = 6465446464544484464541542215456651n
+const num = 23
+console.log(huge * BigInt(num))
+
+// Exceptions
+console.log(20n > 15)
+console.log(20n === 20)
+console.log(typeof 20n)
+console.log(20n == '20')
+
+console.log(huge + ' is REALLY big!!!')
+
+// Divisions
+console.log(11n / 3n)
+console.log(10 / 3)
