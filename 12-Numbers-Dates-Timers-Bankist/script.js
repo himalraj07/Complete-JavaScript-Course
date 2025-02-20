1ddd791c6cd9d2a9a3f197d64d1557b8a37819f8
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-02-20 20:50:08 +0545

    creating dates

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -379,7 +379,6 @@ console.log(PI)
 console.log(Number('230_000'))
 console.log(parseInt('230_000'))
 
-*/
 
 console.log(2 ** 53 - 1)
 console.log(Number.MAX_SAFE_INTEGER)
@@ -411,3 +410,39 @@ console.log(huge + ' is REALLY big!!!')
 // Divisions
 console.log(11n / 3n)
 console.log(10 / 3)
+*/
+/*
+// Create a date
+const now = new Date()
+console.log(now)
+
+console.log(new Date('Feb 20 2025 20:33:30'))
+console.log(new Date('December 24, 2015'))
+console.log(new Date(account1.movementsDates[0]))
+
+console.log(new Date(2037, 10, 19, 15, 23, 5))
+console.log(new Date(2037, 10, 31))
+
+console.log(new Date(0))
+console.log(new Date(3* 24 * 60 * 60 *1000))
+*/
+
+// Working with dates
+const future = new Date(2037, 10, 19, 15, 23, 5)
+console.log(future)
+console.log(future.getFullYear())
+console.log(future.getMonth())
+console.log(future.getDate())
+console.log(future.getDay())
+console.log(future.getHours())
+console.log(future.getMinutes())
+console.log(future.getSeconds())
+console.log(future.toISOString())
+console.log(future.getTime())
+
+console.log(new Date(2142236285000))
+
+console.log(Date.now())
+
+future.setFullYear(2040)
+console.log(future)
