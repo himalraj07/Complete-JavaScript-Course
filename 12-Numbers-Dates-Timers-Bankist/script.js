f0dc13a0dda2bd361f660989017905e13c5be1b8
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-04-30 23:43:01 +0545

    implementing a countdown timer

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -185,14 +185,43 @@ const updateUI = function (acc) {
   calcDisplaySummary(acc)
 }
 
+const startLogOutTimer = function () {
+  const tick = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0)
+    const sec = String(time % 60).padStart(2, 0)
+
+    // In each call, print the remaining time to UI
+    labelTimer.textContent = `${min}:${sec}`
+
+    // When 0 seconds, stop timmer and log out user
+    if (time === 0) {
+      clearInterval(timer)
+      labelWelcome.textContent = `Log in to get started`
+      containerApp.style.opacity = 0
+    }
+
+    // Decrease 1s
+    time--
+  }
+
+  // Set time to 5 minutes
+  let time = 120
+
+  // Call the timer every second
+  tick()
+  const timer = setInterval(tick, 1000)
+
+  return timer
+}
+
 ///////////////////////////////////////
 // Event handlers
-let currentAccount
+let currentAccount, timer
 
 // FAKE ALWAYS LOGGED IN
-currentAccount = account1
-updateUI(currentAccount)
-containerApp.style.opacity = 100
+// currentAccount = account1
+// updateUI(currentAccount)
+// containerApp.style.opacity = 100
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -239,6 +268,11 @@ btnLogin.addEventListener('click', function (e) {
     inputLoginUsername.value = inputLoginPin.value = ''
     inputLoginPin.blur()
 
+    // Timer
+    if (timer) clearInterval(timer)
+
+    timer = startLogOutTimer()
+
     // Update UI
     updateUI(currentAccount)
   }
@@ -268,6 +302,10 @@ btnTransfer.addEventListener('click', function (e) {
 
     // Update UI
     updateUI(currentAccount)
+
+    // Reset timer
+    clearInterval(timer)
+    timer = startLogOutTimer()
   }
 })
 
@@ -286,6 +324,10 @@ btnLoan.addEventListener('click', function (e) {
 
       // Update UI
       updateUI(currentAccount)
+
+      // Reset timer
+      clearInterval(timer)
+      timer = startLogOutTimer()
     }, 2500)
   }
   inputLoanAmount.value = ''
@@ -547,7 +589,6 @@ console.log(
   navigator.language,
   new Intl.NumberFormat(navigator.language, options).format(num)
 )
-*/
 
 // setTimeout
 const ingredients = ['olives', 'spinach']
@@ -565,3 +606,4 @@ setInterval(function () {
   const now = new Date()
   console.log(now)
 }, 1000)
+*/
