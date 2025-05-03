79148dbd8cbb3e78bcecd86f4f69a1b66be2f87d
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-05-03 12:42:31 +0545

    Event propagation in practice

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -149,6 +149,7 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' })
 })
 
+/*
 // Types of events and event handlers
 const h1 = document.querySelector('h1')
 const alertH1 = function (e) {
@@ -164,3 +165,31 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000)
 // }
 
 // Event propagation: bubbling and capturing
+*/
+
+// Event propagation in practice
+
+// rgb(255, 255, 255)
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor()
+  console.log('LINK', e.target, e.currentTarget)
+  console.log(e.target === this)
+
+  // Stop propagation
+  // e.stopPropagation()
+})
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor()
+  console.log('CONTAINER', e.target, e.currentTarget)
+})
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor()
+  console.log('NAV', e.target, e.currentTarget)
+})
