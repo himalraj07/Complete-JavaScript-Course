6e8da71bfda4df41d528e9ed2575e4389b675204
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-05-04 08:43:00 +0545

    passing arguments to event handlers

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -6,6 +6,10 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.btn--close-modal')
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal')
+const nav = document.querySelector('.nav')
+const tabs = document.querySelectorAll('.operations__tab')
+const tabsContainer = document.querySelector('.operations__tab-container')
+const tabsContent = document.querySelectorAll('.operations__content')
 
 ///////////////////////////////////////
 // Modal window
@@ -93,9 +97,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 })
 
 // Building a tabbed component
-const tabs = document.querySelectorAll('.operations__tab')
-const tabsContainer = document.querySelector('.operations__tab-container')
-const tabsContent = document.querySelectorAll('.operations__content')
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab')
@@ -115,6 +116,23 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active')
 })
+
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link')
+    const logo = link.closest('.nav').querySelector('img')
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this
+    })
+    logo.style.opacity = this
+  }
+}
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5))
+nav.addEventListener('mouseout', handleHover.bind(1))
 
 /*
 // Selecting elements
