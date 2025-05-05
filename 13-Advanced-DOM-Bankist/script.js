bc6d5ad39f4e9222d98833e1f32511503d3fff1a
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-05-05 18:26:41 +0545

    efficient script loading: defer and async

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -475,11 +475,11 @@ console.log(h1.parentElement.children)
 
 // Lifecycle DOM events
 document.addEventListener('DOMContentLoaded', function (e) {
-  console.log('HTML parsed and DOM tree built!', e)
+  // console.log('HTML parsed and DOM tree built!', e)
 })
 
 window.addEventListener('load', function (e) {
-  console.log('Page fully loaded', e)
+  // console.log('Page fully loaded', e)
 })
 
 // window.addEventListener('beforeunload', function (e) {
