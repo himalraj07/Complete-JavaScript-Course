c007d19367c3ae9151f2e4272c1627cb4daf2800
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-05-18 09:49:09 +0545

    Using the Geolocation API to get the user's current position

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -10,3 +10,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Using the Geolocation API to get the user's current position
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(`http://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
