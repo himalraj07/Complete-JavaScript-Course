8f5ea9c615f7922242366064a13741ef3d991861
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-05-29 11:37:27 +0545

    listening for load and hashchange events

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -31,11 +31,15 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return; // No recipe id in URL
+
     // Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      // 'https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e84e2'
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`,
     );
     const data = await res.json();
 
@@ -148,4 +152,7 @@ const showRecipe = async function () {
     // alert(err);
   }
 };
-showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
