caa2e4b54f00601a7e92d89a06716d594d26f356
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-05-30 11:25:40 +0545

    implementing search results - part 1

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -1,20 +1,13 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import SearchView from './views/searchView.js';
 
-import 'core-js/stable'; // Polyfills for stable features
-import 'regenerator-runtime/runtime'; // Polyfills for async/await
-import recipeView from './views/recipeView.js';
-
-const recipeContainer = document.querySelector('.recipe');
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -30,7 +23,24 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    // 1. Get query from view
+    const query = SearchView.getQuery();
+    if (!query) return;
+
+    // 2. Load search results
+    await model.loadSearchResults(query);
+
+    // 3. Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  SearchView.addHandlerSearch(controlSearchResults);
 };
 init();
