829b88aa167059c04a9f5a6243d935a6c5e0851e
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-05-30 12:34:53 +0545

    implementing search results - part 2

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -1,9 +1,14 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import SearchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -25,6 +30,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1. Get query from view
     const query = SearchView.getQuery();
     if (!query) return;
@@ -33,7 +40,8 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
 
     // 3. Render results
-    console.log(model.state.search.results);
+    // console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
