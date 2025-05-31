10f4e0213867a124e6bcd294fa69e7fce10acb88
Himal Raj Bhusal <139055215+himalraj07@users.noreply.github.com>
2025-05-31 10:21:35 +0545

    storing bookmarks with localstorage

--- 18-forkify/src/js/views/resultsView.js
+++ 18-forkify/src/js/views/resultsView.js
@@ -7,7 +7,6 @@ class resultsView extends View {
   _message = '';
 
   _generateMarkup() {
-    console.log(this._data);
     return this._data
       .map(result => previewView.render(result, false))
       .join('');
